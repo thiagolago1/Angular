94b29beb0df0cd8ea94cd81d89e62f5745b06430
Thiago Lago <thiagolagoo2@gmail.com>
2019-02-13 22:34:57 -0200

    second commit

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  public to_dos: any[] = [];
+
+  constructor() {
+    this.to_dos.push({ title: 'Arrumar o armário ', done: false });
+    this.to_dos.push({ title: 'Atualizar KanBan', done: false });
+    this.to_dos.push({ title: 'Formatar Desktop', done: false });
+  }
 }
